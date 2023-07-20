3517b70035413baf17d3a98bb62d86ab19ee8c42
kazuwombat <matsumotokazuya7@gmail.com>
2023-07-20 20:36:08 +0900

    execution agent

--- agents/execution_agent.py
+++ agents/execution_agent.py
@@ -1,14 +1,19 @@
+import openai
 def execute(
-  fixed_prompt: str
+  prompt: str
 ) -> str:
   """
-  Execute a fixed prompt and return the output
+  Execute a prompt and return the output
 
   Paramters:
-    fixed_prompt: str
+    propmt: str
   
   Returns:
     str: output of the fixed prompt
   """
-  print("fixed prompt")
-  return 'output'
+  response = openai.ChatCompletion.create( #type: ignore
+    model="gpt-4-0613",
+    messages=[{"role": "system", "content": prompt }],
+    temperature=0
+  )
+  return response["choices"][0]["message"].content #type: ignore
--- prompt_agent.py
+++ prompt_agent.py
@@ -1,10 +1,15 @@
-from agents.prompt_agent import fix_prompt
+# from agents.prompt_agent import fix_prompt
+# from agents.evaluation_agent import evaluate
+from agents.execution_agent import execute
 
 current_prompt = "こんにちは"
 improvments = ['足し算をするようにお願いしましょう']
 
-fixed_prompt = fix_prompt(
-  current_prompt=current_prompt,
-  improvements=improvments
-)
-print("fixed_prompt", fixed_prompt)
+# evaluate("こんにちは", "こんにちは")
+output = execute("こんにちは")
+print("output", output)
+# fixed_prompt = fix_prompt(
+#   current_prompt=current_prompt,
+#   improvements=improvments
+# )
+# print("fixed_prompt", fixed_prompt)
