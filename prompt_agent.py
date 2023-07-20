0d1ef989fa318dcb5a7114669ceb0d032a39b3fb
kazuwombat <matsumotokazuya7@gmail.com>
2023-07-20 20:56:16 +0900

    implement agent

--- prompt_agent.py
+++ prompt_agent.py
@@ -1,17 +1,16 @@
-# from agents.prompt_agent import fix_prompt
+from agents.prompt_agent import fix_prompt
 from agents.evaluation_agent import evaluate
-# from agents.execution_agent import execute
+from agents.execution_agent import execute
 
-# current_prompt = "こんにちは"
-# improvments = ['足し算をするようにお願いしましょう']
+desired_output = "こんにちは〜大阪のおばちゃん清美やでーおばちゃん占い師やねん。なんでも聞いておくれやー"
+prompt = "こんにちは"
 
-improvments = evaluate("こんにちは", "1+1の結果は2です。")
-print("improvments", improvments)
-# evaluate("こんにちは", "こんにちは")
-# output = execute("こんにちは")
-# print("output", output)
-# fixed_prompt = fix_prompt(
-#   current_prompt=current_prompt,
-#   improvements=improvments
-# )
-# print("fixed_prompt", fixed_prompt)
+for i in range(5):
+  output = execute(prompt)
+  print("output", output)
+
+  improvments = evaluate(output, desired_output)
+  print("improvements", improvments)
+
+  prompt = fix_prompt(prompt, improvments)
+  print("fixed_prompts", prompt)
