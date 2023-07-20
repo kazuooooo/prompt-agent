958bf668688ea0c297a1ceb976c0c2b38907a083
kazuwombat <matsumotokazuya7@gmail.com>
2023-07-20 19:25:20 +0900

    Init

---
+++ prompt_agent.py
@@ -0,0 +1,10 @@
+import openai
+
+# create a chat completion
+chat_completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[{"role": "user", "content": "Hello world"}
+])
+
+# print the chat completion
+print(chat_completion.choices[0].message.content)
